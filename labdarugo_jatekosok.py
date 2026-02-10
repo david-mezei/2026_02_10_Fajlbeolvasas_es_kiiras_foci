d684b077261402bd87c648961ac4189cc2202833
Szabolcs Juhász <183376403+juhasz-szabolcs@users.noreply.github.com>
2026-02-10 22:28:34 +0100

    init project

---
+++ labdarugo_jatekosok.py
@@ -0,0 +1,23 @@
+"""
+Olvasd be a labdarugok.txt adatait, majd oldd meg az alábbi feladatokat!
+
+1. Hány játékos szerepel a fájlban?
+2. Melyik játékos szerezte a legkevesebb gólt?
+3. Melyik játékos szerzett a legtöbb gólt?
+4. Ki játszott a legtöbb mérkőzést?
+5. Átlagosan hány gólt szerzett egy játékos?
+
+***EXTRA - nehezebb feladat*** (nem kötelező, de érdemes megpróbálni):
+6. Melyik csapat szerzett a legtöbb gólt? (feltételezve, hogy egy játékos csak egy csapatban játszott)
+
+
+A megoldott feladatokat a kiirt_adatok nevű mappában hozd létre statisztika.txt néven!
+"""
+
+
+print("A beolvasott fájlban összesen ____ játékos szerepel.")
+print("A legkevesebb gólt szerző játékos: ____")
+print("A legtöbb gólt szerző játékos: ____")
+print("A legtöbb mérkőzést játszó játékos: ____")
+print("Az átlagos gólszám: ____")
+print("***A legtöbb gólt szerző csapat: ____")
