a04298f3a93b85eef81f4ab5bce833171fb32dcb
Dávid Mezei <mezei.david.sandor@diak.szbi-pg.hu>
2026-02-11 15:12:41 +0100

    Upload finished project

--- labdarugo_jatekosok.py
+++ labdarugo_jatekosok.py
@@ -14,10 +14,64 @@ Olvasd be a labdarugok.txt adatait, majd oldd meg az alábbi feladatokat!
 A megoldott feladatokat a kiirt_adatok nevű mappában hozd létre statisztika.txt néven!
 """
 
+# Fájl beolvasása
+labdarugok = []
+with open('beolvasando_adatok/labdarugok.txt', 'r', encoding='utf-8') as f:
+    next(f)
+    for sor in f:
+        adatok = sor.strip().split(';')
+        labdarugo = {'nev': adatok[0], 'csapat': adatok[1], 'gol': int(adatok[2]), 'merkozesek': int(adatok[3])}
+        labdarugok.append(labdarugo)
 
-print("A beolvasott fájlban összesen ____ játékos szerepel.")
-print("A legkevesebb gólt szerző játékos: ____")
-print("A legtöbb gólt szerző játékos: ____")
-print("A legtöbb mérkőzést játszó játékos: ____")
-print("Az átlagos gólszám: ____")
-print("***A legtöbb gólt szerző csapat: ____")
+# Kiválogató függvények --> csak hogy ne kelljen 4-szer leírni ugyanazt :D
+def legtobb(adat: str):
+    vizsgalando = labdarugok[0]
+    for labdarugo in labdarugok:
+        if labdarugo[adat] > vizsgalando[adat]:
+            vizsgalando = labdarugo
+    return vizsgalando["nev"]
+
+def legkevesebb(adat: str):
+    vizsgalando = labdarugok[0]
+    for labdarugo in labdarugok:
+        if labdarugo[adat] < vizsgalando[adat]:
+            vizsgalando = labdarugo
+    return vizsgalando["nev"]
+
+
+
+# 1. feladat
+osszes_jatekos = len(labdarugok)
+
+# 2. feladat
+legkevesebb_gol = legkevesebb("gol")
+
+# 3. feladat
+legtobb_gol = legtobb("gol")
+
+# 4. feladat
+legtobb_merkozes = legtobb("merkozesek")
+
+# 5. feladat
+osszes_gol = 0
+for labdarugo in labdarugok:
+    osszes_gol += labdarugo["gol"]
+
+atlag_gol = osszes_gol / osszes_jatekos
+
+
+# Létrehozzuk a mappát, ha még nem létezik.
+from pathlib import Path
+Path("kiirt_adatok").mkdir(exist_ok=True)
+
+with open('kiirt_adatok/statisztika.txt', 'w', encoding='utf-8') as c:
+    c.write(f"A beolvasott fájlban összesen {osszes_jatekos} játékos szerepel.\n")
+    c.write(f"A legkevesebb gólt szerző játékos: {legkevesebb_gol}\n")
+    c.write(f"A legtöbb gólt szerző játékos: {legtobb_gol}\n")
+    c.write(f"A legtöbb mérkőzést játszó játékos: {legtobb_merkozes}\n")
+    c.write(f"Az átlagos gólszám: {atlag_gol:.2f}")
+
+if Path("kiirt_adatok/statisztika.txt").exists:
+    print("A statisztika.txt fájl létrejött, a program kilép.")
+else:
+    print("A fájl nem jött létre. A program kilép.")
